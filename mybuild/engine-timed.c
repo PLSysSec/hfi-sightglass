175ddbbc66743393bfac8d399a496819c9377acf
Shravan Narayan <shravanrn@gmail.com>
2022-10-16 04:11:46 -0400

    Simplify how timings are saved

--- mybuild/engine-timed.c
+++ mybuild/engine-timed.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <inttypes.h>
 #include <time.h>
 #include <unistd.h>
@@ -39,11 +40,26 @@ void Z_benchZ_endZ_vv() {
     int64_t ns = (stop.tv_sec - start.tv_sec) * BILLION  + (stop.tv_nsec - start.tv_nsec);
     printf("!!!Capture_Time(ns): %" PRIi64 "\n", ns);
 
-    const char* outfile = getenv("SIGHTGLASS_OUTPUTFILE");
-    if (outfile) {
-        FILE *fp = fopen(outfile, "w");
+    // SIGHTGLASS_WRITEOUTPUT="1"
+    // SIGHTGLASS_OUTPUTFOLDER="/tmp/"
+    // SIGHTGLASS_OUTPUTFILE="shootout-heapsort_guardpages"
+    const char* writeoutput = getenv("SIGHTGLASS_WRITEOUTPUT");
+    const char* outputfile = getenv("SIGHTGLASS_OUTPUTFILE");
+    const char* outputfolder = getenv("SIGHTGLASS_OUTPUTFOLDER");
+    if (writeoutput) {
+        if (!outputfile || !outputfolder) {
+            printf("Could not save output. Expected SIGHTGLASS_OUTPUTFILE and SIGHTGLASS_OUTPUTFOLDER\n");
+            abort();
+        }
+
+        char path[300];
+        strcpy(path, outputfolder);
+        strcat(path, "/");
+        strcat(path, outputfile);
+
+        FILE *fp = fopen(path, "w");
         if(fp == NULL) {
-            printf("%s file can't be opened\n", outfile);
+            printf("%s file can't be opened\n", path);
             abort();
         }
         fprintf(fp, "%" PRIi64 , ns);
